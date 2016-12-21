4b1f7ef2a879bce599a3aae3a7d1aec628f65924
TJ DeVries <tim.devries@dornerworks.com>
2016-12-21 13:57:13 -0500

    Update rplugin source

--- rplugin/python3/deoplete/langserver.py
+++ rplugin/python3/deoplete/langserver.py
@@ -6,7 +6,7 @@ class Source(Base):
         super(Source, self).__init__(nvim)
 
         self.nvim = nvim
-        self.name = langserver
+        self.name = 'langserver'
         self.mark = '[LSP]'
 
     def on_event(self, context, filename=''):
@@ -15,7 +15,7 @@ class Source(Base):
     def gather_candidates(self, context):
         user_input = context['input']
         filetype = context.get('filetype', '')
-        complete_str  context['complete_str']
+        complete_str = context['complete_str']
 
         return [
             {
